9a22bad1568b9dcbe9f6e6202257ec3e32f528b5
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-18 14:10:03 -0700

    Hello.h

---
+++ hello/hello.h
@@ -0,0 +1,9 @@
+/******************************************************************
+ * PLibC is free software, distribute it under the GPLv3 license. *
+ ******************************************************************/
+#ifndef HELLO_HELLO_H
+#define HELLO_HELLO_H
+
+void hello(void);
+
+#endif /* HELLO_HELLO_H */
